b3f16a565acff6f293a48c818c5426da0b7b27b7
Victor Wilker <wilkersantos125@gmail.com>
2020-01-30 01:28:19 -0300

    create autoload model in index

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set('useCreateIndex', true);
 const UserSchema = new mongoose.Schema({
     name:   {
         type: String,
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const app = require('express')();
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
 
 const  dbConfig = require('./config/database');
 
 
 mongoose.connect( dbConfig.url,{ useNewUrlParser:true , useUnifiedTopology: true});
 
+requireDir(dbConfig.modelsPath);
+
+
+//test db create- Ok
+// const User = mongoose.model('User');
+// console.log(User);
+// User.create({
+//     name:'Victor',
+//     username:'vwso',
+//     email:'wilkersantos125@gmail.com',
+//     password:'teste123'
+//     }, () => {
+//         console.log('OK');
+//     }
+// );
 
 app.listen(3000);
